c710db23e5ae641d59100ad5ba13934307a89a6b
mumu-java <140181017+mumu-java@users.noreply.github.com>
2023-08-05 17:34:43 +0900

    Add files via upload

--- StreamCode2.java
+++ StreamCode2.java
@@ -1,9 +1,10 @@
+//コードを整形しました。
 import java.util.List;
 
-public class StreamCode2 {
+public class StreamCode {
 
     public static void main(String[] args) {
         List<Integer> intList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        intList.stream().filter(i -> i % 2 == 0).map(i -> i * i).forEach(System.out::println);
+        intList.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
     }
 }
